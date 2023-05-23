bb2e3ad599237624cf491547b95d0d00883d4539
Abby O'Donoghue <abigail.odonoghue@bts.tech>
2023-05-23 11:59:30 +0200

    new file for session 2 of cloud & devOps

---
+++ main.js
@@ -0,0 +1,11 @@
+const myFunction =  (text) => {
+    console.log("You called this function with input: " + text)
+};
+
+function sumTwoNumbers (a, b) {
+    return a + b;
+};
+
+//myFunction("Hello World!");
+const result = sumTwoNumbers(1, 2);
+console.log(result);
