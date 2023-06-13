8a249f720de79e8cea7e251fddc9a2ce0b9ccba1
Abby O'Donoghue <abigail.odonoghue@bts.tech>
2023-06-13 09:52:51 +0100

    typescript advantages

---
+++ assignment_session5/theory.js
@@ -0,0 +1,5 @@
+//Could you explain me if Typescript brings any advantage from your point of view?
+//Typescript has a compiler, which provides type and error checking when coding. 
+//It checks for type validity and shows errors in real time. It’s useful for refactoring code, 
+//because the compiler shows users things they may have missed.
+
