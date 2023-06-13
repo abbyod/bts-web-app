2313367ac136d793e1b28b4950f8aa358322b088
Abby O'Donoghue <abigail.odonoghue@bts.tech>
2023-06-13 10:27:00 +0100

    q2

--- assignment_session5/theory.js
+++ assignment_session5/theory.js
@@ -1,5 +1,13 @@
-//Could you explain me if Typescript brings any advantage from your point of view?
+//Q1. Could you explain me if Typescript brings any advantage from your point of view?
+
 //Typescript has a compiler, which provides type and error checking when coding. 
 //It checks for type validity and shows errors in real time. It’s useful for refactoring code, 
 //because the compiler shows users things they may have missed.
 
+//Q2. One of the differences between Vue and React, is that React uses JSX, after doing some research, 
+//could you explain me your opinion about JSX?
+
+//With JSX, you can write code that looks very similar to HTML, but you have the power of
+//mixing JavaScript methods and variables into your code.
+//Developers can easily embed pieces of JS into templates without having to learn an additional language. 
+//Anyone familiar with HTML, CSS, or JS should have no problem reading and understanding JSX 
